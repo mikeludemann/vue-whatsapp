58d4e0412250d186013d1c69ddcb73a1f35f4d99
Mike Ludemann <mike-ludemann@hotmail.de>
2021-01-19 17:21:25 +0100

    Setup complete - vue-whatsapp

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # vue-whatsapp
+
 Vue WhatsApp Component
+
+## Properties
+
+### WhatsApp
+
+|Key|Value|Type|
+|:--|:----|:---|
+|tel|Telephone of the contact|String|
+|text|Text of the message|String|
+|source|Source to the contact|String|
+|data|Data of the settings|String|
+|linktext|Default text of the content|String|
---
+++ src/components/whatsapp.vue
@@ -0,0 +1,56 @@
+<template lang="html">
+	<div id="whatsapp">
+		<a id="content" target="_blank">
+			<div>
+				<i class="fab fa-whatsapp fa-2x">{{this.defaulttext}}</i>
+			</div>
+		</a>
+	</div>
+</template>
+
+<script lang="js">
+	export default {
+		name: 'whatsapp',
+		props: {
+			telephone: String,
+			text: String,
+			source: String,
+			data: String,
+			defaulttext: String
+		},
+		mounted() {
+			this.insertExternalSource();
+		},
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+			insertExternalSource: function () {
+
+				var link = document.createElement("link");
+
+				link.setAttribute("rel", "stylesheet");
+				link.setAttribute("href", "https://use.fontawesome.com/releases/v5.2.0/css/all.css");
+				link.setAttribute("crossorigin", "anonymous");
+				link.setAttribute("integrity", "sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ");
+
+				document.getElementsByTagName("head").item(0).appendChild(link);
+
+				var elem = document.getElementById("content");
+				elem.setAttribute("href", "https://api.whatsapp.com/send?phone=" + this.telephone + "&text=" + this.text + "&source=" + this.source + "&data=" + this.data);
+
+			}
+		},
+		computed: {
+
+		}
+}
+</script>
+
+<style scoped lang="scss">
+	.whatsapp {
+
+	}
+</style>
